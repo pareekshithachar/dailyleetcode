7d7de800914b4bf9377969fc7ca7a573d0348da7
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2025-02-25 09:54:44 +0530

    Time: 249 ms (27.54%), Space: 182.9 MB (27.75%) - LeetHub

--- 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -1,75 +1,61 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&mp, int node, vector<int>&price,  unordered_map<int,int>&parent,vector<int>&amount){
-        // if(mp[node].size() == 1 && parent[node] == mp[node][0]) return amount[node];
+    int findCostWOBob(unordered_map<int,vector<int>>&mp, int node,vector<int>&parent,vector<int>&costwobob,vector<int>& amount){
         
-        int ans = amount[node];
-        int nextbest = INT_MIN;
+        int cost = INT_MIN;
         for(auto x : mp[node]){
-            
-            if(parent.find(x) == parent.end()){
-                
+            if(x != parent[node]){
                 parent[x] = node;
-                nextbest = max(nextbest,dfs(mp,x,price,parent,amount));
-            }   
+                cost= max(cost,findCostWOBob(mp,x,parent,costwobob,amount));
+            }
         }
-        nextbest = nextbest == INT_MIN? 0 : nextbest;
-        return price[node] = ans+ nextbest;
+        if(cost == INT_MIN) cost = 0;
+        cost+=amount[node];
+        return costwobob[node] = cost;
     }
-    int FindBestExceptSpecificChild (vector<vector<int>>&g,unordered_map<int,int>&parent,vector<int>&price,int root, int child_to_skip) {
-        int result = -1e9;
+    int findCostWithoutBobIntersection(unordered_map<int,vector<int>>&mp,vector<int>&costwobob,int cur,int nodeToIgnore, vector<int>&parent){
+        int cost = -1e9;
         int child_count = 0;
-        
-        for (auto child : g[root]) {
-            if (child == parent[root]) continue;
-            
-            child_count ++;
-            if (child == child_to_skip) continue;
-            
-            result = max (result, price[child]);
+        for(auto x : mp[cur]){
+            if(x == parent[cur]){
+                continue;
+            }
+            child_count++;
+            if(x == nodeToIgnore) continue;
+            cost = max(costwobob[x],cost);
         }
-        
         if (child_count == 0) return 0;
-        return result;
+
+        return cost;
     }
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-        // unordered_map<int, list<int>>mp;
-        vector<vector<int>>mp(amount.size());
-        for(int i =0;i<edges.size();i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            mp[u].push_back(v);
-            mp[v].push_back(u);  
+        unordered_map<int,vector<int>>mp;
+        int n = amount.size();
+        for(auto x : edges){
+            mp[x[0]].push_back(x[1]);
+            mp[x[1]].push_back(x[0]);
         }
-        unordered_map<int,int>parent;
-        parent[0] = -1;
-        vector<int>price(mp.size(),-1);
-        // price[0] = amount[0];
-        dfs(mp,0,price,parent,amount);
-        for(auto x : price){
-            cout<<x;
+        vector<int>parent(n,-1);
+        vector<int>costwobob(n,INT_MIN);
+        findCostWOBob(mp,0,parent,costwobob,amount);
+        vector<int>pathofbob;
+        while(bob!=-1){
+            pathofbob.push_back(bob);
+            bob  = parent[bob];
         }
-        vector<int>path;
-        while(bob != -1){
-            path.push_back(bob);
-            bob = parent[bob];
+        reverse(pathofbob.begin(),pathofbob.end());
+        int ans = INT_MIN;
+        n= pathofbob.size();
+        for(int i =0, cost=0;i<pathofbob.size();i++){
+            if(i < n-1-i){
+                cost += amount[pathofbob[i]];
+            }
+            if( i == n-1-i){
+                cost += amount[pathofbob[i]]/2;
+            }
+            ans = max(ans,cost + findCostWithoutBobIntersection(mp,costwobob,pathofbob[i],i+1 < n ?pathofbob[i+1] : -1,parent));
         }
-        reverse(path.begin(),path.end());
-        int result = INT_MIN;
-        int n = path.size();
-        for (int ind = 0, cost_till_now = 0; ind < n; ind ++) {
-            bool is_alice_first = (ind < n-1-ind);
-            bool is_same_arrival = (ind == n-1-ind);
-            
-            if (is_alice_first)  cost_till_now += amount[path[ind]];
-            if (is_same_arrival) cost_till_now += amount[path[ind]] / 2;
-            
-            result = max (
-                result, 
-                cost_till_now + FindBestExceptSpecificChild (mp,parent,price,path[ind], ind+1 < n? path[ind+1] : -1)
-            );
-        }
-        
-        return result;
+
+        return ans;
     }
 };
