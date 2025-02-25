b46a4bd49f6e7581e1b694d4eaa9025a0d98c4e1
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2025-02-25 10:39:36 +0530

    Time: 0 ms (100%), Space: 112.2 MB (32.08%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,35 +1,19 @@
-#define MOD 1000000007
 class Solution {
-
 public:
     int numOfSubarrays(vector<int>& arr) {
-        int ans = 0;
-        int n = arr.size();
-        vector<int>presum(n,0);
-        presum[0] = arr[0];
-        for(int i=1;i<arr.size();i++){
-            presum[i] = presum[i-1]+arr[i];
-        }
-        int evensum=0;
-        int oddsum=0;
-        for(int i =0;i<n;i++){
-            if(presum[i]%2){
-                oddsum++;
+        long ans = 0;
+        int odd = 0,even=0;
+        int sum = 0;
+        for(int x : arr){
+            sum+=x;
+            if(sum%2){
+                ans+= even+1;
+                odd++;
             }else{
-                evensum++;
+                ans+=odd;
+                even++;
             }
-            if(presum[i]%2){
-                ans += (evensum+1)%MOD;
-            }else{
-                ans+= oddsum%MOD;
-            }
-            ans=ans%MOD;
         }
-        return ans;
-        
+        return ans%1000000007;
     }
 };
-
-
-//1 5 1 3 5 6
-//1 6 7 10 15 21
