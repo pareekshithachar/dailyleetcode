dddf04817adbe9d05e46c1f55cbab22147bb8a9e
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2025-02-26 09:59:39 +0530

    Time: 0 ms (100%), Space: 45.1 MB (78.26%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int ans=0,negans = 0;
-        int psum=0,nsum=0;
+        int ans=0;
+        int prefix=0;
+        int minprefix = 0;
+        int maxprefix = 0;
         for(int i=0;i<nums.size();i++){
-            psum+=nums[i];
-            ans=max(psum,ans);
-            if(psum < 0){
-                psum=0;
-            }
-            nsum+=nums[i];
-            negans = min(negans,nsum);
-            if(nsum>0) nsum = 0;
+            prefix+=nums[i];
+            minprefix = min(prefix,minprefix);
+            maxprefix = max(prefix,maxprefix);
         }
-        ans = max(ans,abs(negans));
-        return ans;
+        
+        return maxprefix-minprefix;
     }
 };
