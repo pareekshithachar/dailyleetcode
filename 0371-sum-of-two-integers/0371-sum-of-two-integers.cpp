bff3f8e9cd3d3ec976c7b8309ef2af7fc5ce0ca0
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2026-02-20 19:23:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (94.36%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -3,20 +3,21 @@ public:
     int getSum(int a, int b) {
         int carry = 0;
         int ans = 0;
-        for(int i = 0;i<32;i++){
-            int l = 1&(a>>i);
-            int r = 1&(b>>i);
-            int temp = l + r;
-            if(carry == 1){
-                temp+= 1;
-                carry = 0;
-            }
-            if(temp == 2){
-                temp = carry ? 1 : 0;
-                carry = 1;     
-            }
-            ans = ans|(temp<<i);
+
+        for (int i = 0; i < 32; i++) {
+            int l = (a >> i) & 1;
+            int r = (b >> i) & 1;
+
+            // Correct sum bit
+            int sum = l ^ r ^ carry;
+
+            // Set result bit
+            ans |= (sum << i);
+
+            // Correct carry computation
+            carry = (l & r) | (l & carry) | (r & carry);
         }
+
         return ans;
     }
 };
