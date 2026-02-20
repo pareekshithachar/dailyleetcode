8e077bb3b9eac872d97bee5ce6861354d1a475a0
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2026-02-20 19:40:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (32.71%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int carry = 0;
-        int ans = 0;
-
-        for (int i = 0; i < 32; i++) {
-            int l = (a >> i) & 1;
-            int r = (b >> i) & 1;
-
-            // Correct sum bit
-            int sum = l ^ r ^ carry;
-
-            // Set result bit
-            ans |= (sum << i);
-
-            // Correct carry computation
-            carry = (l & r) | (l & carry) | (r & carry);
+        while(b){
+            unsigned int carry = (unsigned)(a&b)<<1;
+            a = a^b;
+            b = carry;
         }
-
-        return ans;
+        return a;
     }
 };
